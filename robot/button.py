b44f3480b0b11422d4cb96bd535b74a3134731ef
Christophe Vauclair <cfvauclair@gmail.com>
2017-03-26 01:14:00 +0000

    Robot loop script
    -Script to run on a loop on the raspberry pi. It takes a picture when a button is pressed.

---
+++ robot/button.py
@@ -0,0 +1,39 @@
+# GPIO access
+import RPi.GPIO as GPIO
+
+# Get the time and convert it to string
+import time
+from time import strftime, gmtime
+
+# To run command (raspistill)
+from subprocess import call
+
+# To connect to FTP
+from ftplib import FTP
+
+# To run code on interrupt
+#import signal
+#import sys
+
+#def cleanup(signal, frame):
+#	print("Cleaning up...")
+#	ftp.quit()
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+ftp = FTP("192.168.43.224")
+ftp.login("robot","robohacks")
+ftp.cwd("face-request")
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+        print('Taking picture')
+	s = strftime(("%H-%M-%S"), gmtime())
+	call(["raspistill","-o",s + ".jpg"])
+	ftp.
+        time.sleep(0.2)
+
+#signal.signal(signal.SIGINT, cleanup)
